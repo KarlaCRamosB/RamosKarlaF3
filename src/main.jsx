95f3631b4b601fe3d97dc7a2f22ac7bdef60279c
KarlaCRamosB <kandy.buenrostro1@gmail.com>
2024-12-07 11:46:25 -0300

    modificacion para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./Components/utils/global.context"; //ContextGlobal
+//import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
